2e274411a18aa92fe209fe39a043c7b1086237aa
Felix David Ochoa de Hoyos <81578874+FelixOchoa@users.noreply.github.com>
2023-11-23 16:47:37 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    API_AERIS_BACKEND: "http://31.220.101.22:3100",
+    API_AERIS_BACKEND: "https://devmrchofferapi.devzeros.com",
   },
 };
 
